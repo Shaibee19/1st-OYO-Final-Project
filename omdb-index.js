c5dbc4cc212153fbe7f010c43745eca252bbbc10
Shaibee19 <shaipattzgray@gmail.com>
2025-07-09 20:40:25 -0400

    trying to get the search bar working

--- omdb-index.js
+++ omdb-index.js
@@ -1,9 +1,15 @@
 let isModalOpen = false;
 const movieListEl = document.querySelector(".movie-list");
+const id = localStorage.getItem("id");
+
+async function onSearchChange(event) {
+  const id = event.target.value;
+  main(id)
+}
 
 async function main() {
   // const apiKey = "4ea1d0b9"
-  const movies = await fetch("http://www.omdbapi.com/?s=fast&apikey=4ea1d0b9");
+  const movies = await fetch(`http://www.omdbapi.com/?s=${id}&apikey=4ea1d0b9`);
   const moviesData = await movies.json();
   // const searchForm = document.getElementById("search-form");
   // const searchInput = document.getElementById("search-input");
@@ -14,24 +20,18 @@ async function main() {
 
 main();
 
-function displayMovies(imdbID) {
-  console.log(window.location);
-  // window.location.href = `http://127.0.0.1:5500/drivein.html`
-  // console.log(imdbID);
-}
-
 function movieHTML(movie) {
   return `<div class="movie-card" onClick="displayMovies(${movie.imdbID})">
-    <div class="movie-card__container">
-    <img src=${movie.Poster} alt=""/>
-            <div class="movie-card__info">
-            <h4>${movie.Title}</h4>
-            <p><b>Year:</b> ${movie.Year}</p>
-            <p><b>Genre:</b> ${movie.Genre}</p>
-            <p><b>Rating:</b> ${movie.Rating}</p>
-            </div>
-          </div>
-      </div>`;
+  <div class="movie-card__container">
+  <img src=${movie.Poster} alt=""/>
+  <div class="movie-card__info">
+  <h4>${movie.Title}</h4>
+  <p><b>Year:</b> ${movie.Year}</p>
+  <p><b>Genre:</b> ${movie.Genre}</p>
+  <p><b>Rating:</b> ${movie.Rating}</p>
+  </div>
+  </div>
+  </div>`;
 }
 
 // Event listener for search
@@ -58,22 +58,6 @@ function movieHTML(movie) {
 //   }
 // });
 
-// function displayMovies(movies) {
-//   // Save globally for sorting
-//   window.foundMovies = movies;
-
-// document.getElementById("filter").addEventListener("change", function (e) {
-//   const key = e.target.value;
-//   const sorted = [...(window.foundMovies || [])];
-
-//   sorted.sort((a, b) => {
-//     const valA = a[key] ? a[key].toString().toLowerCase() : "";
-//     const valB = b[key] ? b[key].toString().toLowerCase() : "";
-//     return valA.localeCompare(valB);
-//   });
-
-//   displayMovies(sorted);
-// });
 
 const toggleBtn = document.getElementById("theme-toggle");
 
